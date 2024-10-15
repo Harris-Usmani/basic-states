5ea3e802a69b57f6e211af76d5691a5692702913
Harris-Usmani <harrisusmani@protonmail.com>
2024-10-15 17:04:20 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  function toggleButton() {
+    if (isOn) {
+      setIsOn(false); // Set to false if it's currently true
+    } else {
+      setIsOn(true);  // Set to true if it's currently false
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Toggle Button Example</h1>
+      <button onClick={toggleButton}>
+        {isOn ? 'ON' : 'OFF'}
+      </button>
     </div>
   );
 }
